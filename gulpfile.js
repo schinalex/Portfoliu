12b0ad83038801ae35712472db2a99b044c38aed
Alex Schin <alschin@endava.net>
2016-03-18 17:12:25 +0200

    cleaned up a bit

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,9 @@
 'use strict'
 var gulp = require('gulp')
 var autoprefixer = require('gulp-autoprefixer')
-var concatCSS = require('gulp-concat-css')
 var cleanCSS = require('gulp-clean-css')
 var rename = require('gulp-rename')
 var sass = require('gulp-sass')
-var sourcemaps = require('gulp-sourcemaps')
-// var sassIndex = require('sass-index')
 var browserSync = require('browser-sync').create()
 var reload = browserSync.reload
 
@@ -16,36 +13,27 @@ gulp.task('serve', function () {
   })
 })
 
-gulp.task('html', function () {
-  gulp.src('app/*.html')
-  // do html stuff
-})
-
 gulp.task('sass', function () {
-  return gulp.src('sass/*.scss')
-    .pipe(sourcemaps.init())
+  gulp.src('sass/style.sass')
     .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.stream())
 })
 
-gulp.task('css', function () {
-  gulp.src('app/css/*.css')
-  .pipe(concatCSS('bundle.css'))
-	.pipe(autoprefixer({
-  browsers: ['last 2 versions'],
-  cascade: false
-	}))
-  .pipe(cleanCSS())
-  .pipe(rename('style.min.css'))
-  .pipe(gulp.dest('app/css'))
+gulp.task('mincss', function () {
+  gulp.src('app/css/style.css')
+    .pipe(cleanCSS())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('app/css'))
 })
 
 gulp.task('watch', function () {
-  gulp.watch('app/*.html', ['html'])
-  gulp.watch('sass/*.scss', ['sass'])
+  gulp.watch('sass/style.sass', ['sass'])
   gulp.watch('app/*.html').on('change', reload)
 })
 
-gulp.task('default', ['serve', 'html', 'sass', 'css', 'watch'])
+gulp.task('default', ['serve', 'sass', 'watch'])
