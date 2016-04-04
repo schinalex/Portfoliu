e36c65128c9447338e981d241cce920292652968
alex <schinalex@gmail.com>
2016-04-04 17:08:05 +0300

    fixed some bugs

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,24 @@
 // var main = function () {
   'use strict'
+  /* Create a map object and specify the DOM element for display. */
+  var initMap = function () {
+    var myLatLng = {lat: 46.994219, lng: 28.851536}
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: myLatLng,
+      scrollwheel: false,
+      zoom: 9
+    })
+
+    var marker = new google.maps.Marker({
+      map: map,
+      position: myLatLng,
+      title: 'Home'
+    })
+  }
+
+  initMap()
+
   /* Push the body and the nav over by 285px over */
   $('.icon-menu').click(function (event) {
     $('.overlay').show()
@@ -26,8 +45,9 @@
   /* set background and save the choice in cookie */
   var changeBackground = function () {
     var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)background\s*\=\s*([^;]*).*$)|^.*$/, "$1")
-      $('#hello').removeClass('bg1 bg2 bg3')
-      $('#hello').addClass(cookieValue)
+      $('main').removeClass('bg1 bg2 bg3')
+      $('main').addClass(cookieValue)
+
   }
   var selectBackground = function (selectedBackground) {
     document.cookie = 'background=' + selectedBackground
@@ -35,44 +55,11 @@
   }
 
   changeBackground()
-  // jQuery validation
-  // $('#contact-form').validate()
 
-  // Create a map object and specify the DOM element for display.
-  function initMap() {
-      var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 46.994911, lng: 28.854100},
-        scrollwheel: false,
-        zoom: 9
-      })
-    }
+  /* jQuery validation */
+  $('#contact-form').validate()
 
-   initMap()
 
-  // var search = function () {
-  //   // var serachWord = $('#search').val()
-  //   // var jqsections = $('section')
-  //   // console.log(jqsections)
-  //   var serachWord = document.querySelector('#search')
-  //   var sections = document.querySelectorAll('section')
-  //   console.log(sections)
-  //   for (var section in sections) {
-  //     if (sections.hasOwnProperty(section)) {
-  //       console.log(sections[section])
-  //       var elements = section.childNodes
-  //       console.log(elements)
-  //       for (var element of section) {
-  //         // console.log(sections[section].hasOwnProperty(element))
-  //         // if (sections[section].hasOwnProperty(element)) {
-  //           console.log(section)
-  //         // }
-  //       }
-  //     } else {
-  //       console.log('this is not his property: ' + sections[section]);
-  //     }
-  //   }
-  // }
-  // search()
 // }
 //
 // $(document).ready(main)
